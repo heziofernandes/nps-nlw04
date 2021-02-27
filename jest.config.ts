18ea62fd7c4742affb65157a275016fe14ff76f4
Hezio Fernandes <hezinho@Hezios-MacBook-Pro.local>
2021-02-27 17:02:08 +0000

    Refactoring in progress

--- jest.config.ts
+++ jest.config.ts
@@ -145,6 +145,7 @@ export default {
 
   // The test environment that will be used for testing
   //testEnvironment: "node",
+  testEnvironment: "jest-environment-jsdom-fifteen",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
